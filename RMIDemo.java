631e24a87de3352cc69a701f638a1755f504b266
annikenwlie <37176317+annikenwlie@users.noreply.github.com>
2018-03-14 14:08:44 +0000

    Add files via upload

---
+++ RMIDemo.java
@@ -0,0 +1,7 @@
+package rmidemoapp;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+public interface RMIDemo extends Remote {
+	public String doCommunicate(String name) throws RemoteException;
+
+}
