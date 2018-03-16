91da393a86779532debc782a5b14c152a92dad3d
Ricardo Pires <ricardo_pires2@live.com.pt>
2018-03-16 21:40:44 +0000

    Almost All functions done.

--- RMIDemo.java
+++ RMIDemo.java
@@ -1,7 +1,11 @@
-package rmidemoapp;
+
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 public interface RMIDemo extends Remote {
 	public String doCommunicate(String name) throws RemoteException;
-
+	public String sendAmount(String src, String dst, String verification, int amount) throws RemoteException;
+	public String receiveAmount(String src, String dst, String verification, int amount, int id) throws RemoteException;
+	public List<String> checkAccount(String pubKey, String verification) throws RemoteException;
+	public List<String> audit(String pubKey) throws RemoteException;
 }
--- RMIDemoImpl.java
+++ RMIDemoImpl.java
@@ -1,12 +1,96 @@
-package rmidemoapp;
+
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.crypto.NoSuchPaddingException;
+
+import AsymetricEncription.AsymmetricCryptography;
 public class RMIDemoImpl extends UnicastRemoteObject implements RMIDemo{
 	private static final long serialVersionUID = 1L;
-	protected RMIDemoImpl() throws RemoteException{
+	private final AsymmetricCryptography ac;
+	private final MysqlCon db;
+	
+	protected RMIDemoImpl() throws RemoteException, NoSuchAlgorithmException, NoSuchPaddingException{
 		super();
+		ac = new AsymmetricCryptography();
+		db = new MysqlCon();
 	}
+	
+	private boolean verifyKey(String pubKey, String ver){
+		byte[] verification = ver.getBytes();
+		byte[] publicBytes = pubKey.getBytes();
+		
+		X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicBytes);
+		KeyFactory keyFactory;
+		
+		try {
+			keyFactory = KeyFactory.getInstance("RSA");
+			PublicKey pubKey1 = keyFactory.generatePublic(keySpec);
+			String result = ac.Decrypt(pubKey1, verification);
+			return pubKey.equals(result);
+		} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
 	public String doCommunicate (String name) throws RemoteException{
 		return "\nServer says: Hi " +name+ "\n";
 	}
+	@Override
+	public String sendAmount(String src, String dst, String verification, int amount) throws RemoteException {
+		if(!verifyKey(src, verification))
+			return "NACK";
+		
+		int balance = db.getBalance(src) - amount;
+		if(balance < 0)
+			return "NACK";
+		
+		db.updateBalance(src, balance);
+		db.createPendingLedger(src, dst, amount);
+		
+		return "ACK";
+	}
+	@Override
+	public String receiveAmount(String src, String dst, String verification, int amount, int id) throws RemoteException {
+		if(!verifyKey(dst, verification))
+			return "NACK";
+		
+		db.updateBalance(dst, db.getBalance(dst) + amount);
+		db.createAcceptedLedger(src, dst, amount, id);
+		
+		return "ACK";
+		
+	}
+	@Override
+	public List<String> checkAccount(String pubKey, String verification) throws RemoteException {
+		if(!verifyKey(pubKey, verification))
+			return null;
+		
+		ArrayList<String> account = new ArrayList<>(50);
+		String balance = Integer.toString(db.getBalance(pubKey)); 
+		account.add(balance);
+		
+		//db.getIncomingPendingTransfers(pubKey);
+		//FIX ME
+		
+		return account;
+	}
+	@Override
+	public List<String> audit(String pubKey) throws RemoteException {
+		ArrayList<String> ledger = new ArrayList<>(50);
+		
+		//db.getIncomingPendingTransfers(pubKey);
+		//FIX ME
+		
+		return ledger;
+	}
+	
 }
