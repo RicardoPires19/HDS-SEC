9057ff42df424a57b092cbec8e2ce156ed327aa2
annikenwlie <37176317+annikenwlie@users.noreply.github.com>
2018-03-21 20:58:13 +0000

    Update RMIDemo.java
    added nonces

--- RMIDemo.java
+++ RMIDemo.java
@@ -4,8 +4,8 @@ import java.rmi.RemoteException;
 import java.util.List;
 public interface RMIDemo extends Remote {
 	public String doCommunicate(String name) throws RemoteException;
-	public String sendAmount(String src, String dst, String verification, int amount) throws RemoteException;
-	public String receiveAmount(String src, String dst, String verification, int amount, int id) throws RemoteException;
-	public List<String> checkAccount(String pubKey, String verification) throws RemoteException;
+	public String sendAmount(String src, String dst, String verification, int amount, String nonce) throws RemoteException;
+	public String receiveAmount(String src, String dst, String verification, int amount, int id, String nonce) throws RemoteException;
+	public List<String> checkAccount(String pubKey, String verification, String nonce) throws RemoteException;
 	public List<String> audit(String pubKey) throws RemoteException;
 }
