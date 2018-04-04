22cf4a8d23806197237598552310b5de73b33a40
annikenwlie <37176317+annikenwlie@users.noreply.github.com>
2018-04-04 15:03:10 +0100

    Update initMac.java

--- initMac.java
+++ initMac.java
@@ -4,19 +4,19 @@ import javax.crypto.*;
 
 public class initMac {
 
-        public String getMac(String nonce, SecretKey sk) throws Exception {
-        	// Generate secret key for HmacSHA256
-            //KeyGenerator kg = KeyGenerator.getInstance("HmacSHA256");
-            //SecretKey sk = kg.generateKey();
+        public String getMac(String input, SecretKey sk) throws Exception {
+                // Generate secret key for HmacSHA256
+                //KeyGenerator kg = KeyGenerator.getInstance("HmacSHA256");
+                //SecretKey sk = kg.generateKey();
 
-            // Get instance of Mac object implementing HmacSHA256, and
-            // initialize it with the above secret key
-        		Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
-        		sha256_HMAC.init(sk);
-            byte[] result = sha256_HMAC.doFinal(nonce.getBytes());
+                // Get instance of Mac object implementing HmacSHA256, and
+                // initialize it with the above secret key
+                Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+                sha256_HMAC.init(sk);
+                byte[] result = sha256_HMAC.doFinal(input.getBytes());
             
             System.out.println(new String(result));
-            return new String(result);
+            return result;
             
         }
         
